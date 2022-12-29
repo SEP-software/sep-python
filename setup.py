63729197694f99098919e1d474877dc35b84d025
Bob Clapp <bob@sep.stanford.edu>
2022-12-29 20:21:54 +0000

    rename sepPython

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
-setup(name='sep-python',
+setup(name='sepPython',
       version='0.9',
       description='A library for basic IO )SEP) all in python',
       author='R. Clapp',
       author_email='bob@sep.stanford.edu',
       url="http://zapad.stanford.edu/bob/sep-python",
-          packages=['sep-python'],
+          packages=['sepPython'],
     install_requires=[ 'google-cloud-storage>=2.4.0',
                        'genericSolver @ git+https://zapad.stanford.edu/bob/python-solver'
     ]
