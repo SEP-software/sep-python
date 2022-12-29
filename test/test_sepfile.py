6c18b9e57dcd8a40cef49db61cf130925ced6973
Bob Clapp <bob@sep.stanford.edu>
2022-12-29 23:02:09 +0000

    test

--- test/test_sepfile.py
+++ test/test_sepfile.py
@@ -1,5 +1,5 @@
 import pytest
-from sepPython.sepIO import sepIO
+import sepPython.sepIO 
 import os
 from sepPython.Hypercube import hypercube,axis
 import numpy as np
@@ -8,11 +8,11 @@ import logging
 
 
 
-def eest_simpleArgs_1Line():
+def test_simpleArgs_1Line():
     f=open("/tmp/junk.H","w")
     f.write("n1=10 o1=1. d1=2. label='test' in='/tmp/junk.H@'\n")
     f.close()
-    fl=sepIO.sFile(path="/tmp/junk.H")
+    fl=sepPython.sepIO.sFile(path="/tmp/junk.H")
     assert 10==fl.getInt("n1")
     assert 2.==fl.getFloat("d1")
     assert 5.==fl.getFloat("DJFKKDJ",5.)
@@ -20,9 +20,9 @@ def eest_simpleArgs_1Line():
 
 def test_simpleWriteHistory():
     hyper=hypercube(ns=[10,12],os=[2.,1.])
-    fle=sepIO.sFile(hyper=hyper,path="/tmp/junk.H",type="float64")
+    fle=sepPython.sepIO.sFile(hyper=hyper,path="/tmp/junk.H",type="float64")
     fle.writeDescription()
-    fin=sepIO.sFile(path="/tmp/junk.H")
+    fin=sepPython.sepIO.sFile(path="/tmp/junk.H")
     assert 10==fin.getInt("n1")
     assert 12==fin.getInt("n2")
     assert .01 > abs(fin.getFloat("o1")-2.)
@@ -30,9 +30,9 @@ def test_simpleWriteHistory():
 
 def test_simpleWriteHistoryGCS():
     hyper=hypercube(ns=[10,12],os=[2.,1.])
-    fle=sepIO.sGcsObj(hyper=hyper,path="gs://scratch-sep/junk.H",type="float64")
+    fle=sepPython.sepIO.sGcsObj(hyper=hyper,path="gs://scratch-sep/junk.H",type="float64")
     fle.close()
-    fin=sepIO.sGcsObj(path="gs://scratch-sep/junk.H")
+    fin=sepPython.sepIO.sGcsObj(path="gs://scratch-sep/junk.H")
     assert 10==fin.getInt("n1")
     assert 12==fin.getInt("n2")
     assert .01 > abs(fin.getFloat("o1")-2.)
@@ -43,10 +43,10 @@ def test_simpleWriteRead():
     for j in range(ar.shape[0]):
         for i in range(ar.shape[1]):
             ar[j,i]=100*j+i
-    fle=sepIO.sFile(array=ar,path="/tmp/junk2.H")
+    fle=sepPython.sepIO.sFile(array=ar,path="/tmp/junk2.H")
     fle.writeDescription()
     fle.write(ar)
-    fl2=sepIO.sFile(path="/tmp/junk2.H")
+    fl2=sepPython.sepIO.sFile(path="/tmp/junk2.H")
     ar2=np.zeros((20,10))
     fl2.read(ar2)
     for j in range(ar.shape[0]):
@@ -66,12 +66,12 @@ def test_simpleWriteReadGCP():
     for j in range(ar.shape[0]):
         for i in range(ar.shape[1]):
             ar[j,i]=100*j+i
-    fle=sepIO.sGcsObj(array=ar,path="gs://scratch-sep/junk2.H")
+    fle=sepPython.sepIO.sGcsObj(array=ar,path="gs://scratch-sep/junk2.H")
     fle.writeDescription()
     fle.write(ar)
     fle.close()
 
-    fl2=sepIO.sGcsObj(path="gs://scratch-sep/junk2.H")
+    fl2=sepPython.sepIO.sGcsObj(path="gs://scratch-sep/junk2.H")
     ar2=np.zeros((20,10))
     fl2.read(ar2)
     for j in range(ar.shape[0]):
