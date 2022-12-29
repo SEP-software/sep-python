24e2b0a265ea236d616959d1e27cf9af901b02ba
Bob Clapp <bob@sep.stanford.edu>
2022-12-29 22:11:42 +0000

    ee

--- sepPython/__init__.py
+++ sepPython/__init__.py
@@ -1,11 +1,8 @@
 import sepPython.modes
-import sepPython.Hypercube
 import sepPython.sepIO
 
 
 
 
 
-hypercube=sepPython.Hypercube.hypercube
-axis=sepPython.Hypercube.axis
 defaultIO=sepPython.modes.defaultIO
