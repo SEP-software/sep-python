e7fbd070496c732c9f905a61276e492f87ba651a
Bob Clapp <clapp@google.com>
2023-05-20 23:59:23 +0000

    fixed seo

--- sep_python/__init__.py
+++ sep_python/__init__.py
@@ -1,5 +1,5 @@
 
-from seo_python._hypercube import ( Hypercube, Axis)
+from sep_python._hypercube import ( Hypercube, Axis)
 from sep_python._sep_vector import (FloatVector, IntVector, DoubleVector, ComplexDoubleVector, ComplexVector, ByteVector,get_sep_vector)
 from sep_python._modes import default_io
 from sep_python._sep_io import (SEPFile, SEPGcsObj)
